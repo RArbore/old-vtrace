1ac619daa247dcfde27e4be05d61bac0cd8eb5ae
Russel Arbore <russel.jma@gmail.com>
2022-05-02 14:33:33 -0500

    Get FPS

--- src/main.c
+++ src/main.c
@@ -10,11 +10,23 @@
     You should have received a copy of the GNU General Public License
     along with vtrace. If not, see <https://www.gnu.org/licenses/>.  */
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
+#include <time.h>
 
 #include "window.h"
 #include "render.h"
 #include "error.h"
+
+static float get_frame_time() {
+    static long last = ~0;
+    struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    long diff = spec.tv_nsec >= last ? spec.tv_nsec - last : 0;
+    last = spec.tv_nsec;
+    return (float) diff;
+}
 
 int main(void) {
     PROPAGATE(glfwInit() == GLFW_TRUE, ERROR, "Couldn't initialize GLFW.");
@@ -27,6 +39,8 @@ int main(void) {
     PROPAGATE_CLEANUP_END(ERROR);
 
     while (!should_close(&window)) {
+	float dt = (float) 1e9 / get_frame_time();
+	printf("%f\n", dt);
 	glfwPollEvents();
 	PROPAGATE_CLEANUP_BEGIN(render_frame(&window) == SUCCESS, "Failed to render frame.");
 	destroy_window(&window);
