212c7f95d8296226153d1008132f05315230b5d7
Russel Arbore <russel.jma@gmail.com>
2022-05-29 20:20:16 -0500

    Morton code functions

--- src/world.c
+++ src/world.c
@@ -17,104 +17,40 @@ void init_camera(camera_t* camera) {
     memset(&camera->_camera_rot, 0, 2 * sizeof(float));
 }
 
-static const uint32_t SCRATCH_SPACE_MULTIPLIER = 8;
-
-typedef enum svo_node_type_t {
-    PARENT = 0,
-    LEAF = 1,
-    INVALID = 2,
-    SVO_ERROR = 3,
-} svo_node_type_t;
-
-static svo_node_type_t construct_svo_tree(svo_node_t* scratch_arena, uint32_t* num_alloc, uint32_t target, uint32_t max_nodes, uint32_t* voxels, uint32_t w, uint32_t x, uint32_t y, uint32_t z) {
-    if (w == 1) {
-	uint32_t voxel = voxels[x + w * (y + w * z)];
-	scratch_arena[target]._raw = voxel;
-	return voxel ? LEAF : INVALID;
-    }
-    else {
-	svo_node_type_t codes[8];
-	uint32_t start = *num_alloc;
-	scratch_arena[target]._parent._child_pointer = (uint16_t) start;
-	*num_alloc += 8;
-	PROPAGATE(max_nodes > *num_alloc, SVO_ERROR, "Number of allocated nodes exceeded the maximum number of nodes specified.");
-	uint32_t hw = w / 2;
-
-	PROPAGATE(codes[0] = construct_svo_tree(scratch_arena, num_alloc, start, max_nodes, voxels, hw, x, y, z) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[1] = construct_svo_tree(scratch_arena, num_alloc, start + 1, max_nodes, voxels, hw, x + hw, y, z) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[2] = construct_svo_tree(scratch_arena, num_alloc, start + 2, max_nodes, voxels, hw, x, y + hw, z) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[3] = construct_svo_tree(scratch_arena, num_alloc, start + 3, max_nodes, voxels, hw, x + hw, y + hw, z) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[4] = construct_svo_tree(scratch_arena, num_alloc, start + 4, max_nodes, voxels, hw, x, y, z + hw) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[5] = construct_svo_tree(scratch_arena, num_alloc, start + 5, max_nodes, voxels, hw, x + hw, y, z + hw) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[6] = construct_svo_tree(scratch_arena, num_alloc, start + 6, max_nodes, voxels, hw, x, y + hw, z + hw) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-	PROPAGATE(codes[7] = construct_svo_tree(scratch_arena, num_alloc, start + 7, max_nodes, voxels, hw, x + hw, y + hw, z + hw) != SVO_ERROR, SVO_ERROR, "Couldn't create initial sub-SVO structure.");
-
-	for (int8_t i = 0; i < 8; ++i) {
-	    scratch_arena[target]._parent._invalid_mask >>= 1;
-	    scratch_arena[target]._parent._invalid_mask |= 0x80 * codes[i] == INVALID;
-	    scratch_arena[target]._parent._leaf_mask >>= 1;
-	    scratch_arena[target]._parent._leaf_mask |= 0x80 * codes[i] == LEAF;
-	}
-	
-	if (scratch_arena[target]._parent._invalid_mask == 0xFF) {
-	    scratch_arena[target]._raw = 0;
-	    return INVALID;
-	}
-	if (scratch_arena[target]._parent._leaf_mask == 0xFF) {
-	    uint8_t i;
-	    for (i = 0; i < 7; ++i) {
-		if (scratch_arena[start + i]._raw != scratch_arena[start + i + 1]._raw) break;
-	    }
-	    if (i == 7) {
-		scratch_arena[target]._raw = scratch_arena[start]._raw;
-		return LEAF;
-	    }
-	}
-	return PARENT;
-    }
+static uint64_t split_by_3(uint32_t a){
+    uint64_t x = (uint64_t) a;
+    x &= 0x1fffff;
+    x = (x | x << 32) & 0x1f00000000ffff;
+    x = (x | x << 16) & 0x1f0000ff0000ff;
+    x = (x | x << 8) & 0x100f00f00f00f00f;
+    x = (x | x << 4) & 0x10c30c30c30c30c3;
+    x = (x | x << 2) & 0x1249249249249249;
+    return x;
 }
 
-typedef struct svo_queue_entry_t {
-    uint32_t _index;
-    svo_node_type_t _type;
-} svo_queue_entry_t;
+static uint32_t compact_by_3(uint64_t x) {
+    x &= 0x1249249249249249;
+    x = (x | x >> 2) & 0x10c30c30c30c30c3;
+    x = (x | x >> 4) & 0x100f00f00f00f00f;
+    x = (x | x >> 8) & 0x1f0000ff0000ff;
+    x = (x | x >> 16) & 0x1f00000000ffff;
+    x = (x | x >> 32) & 0x1fffff;
+    return (uint32_t) x;
+}
+
+static uint64_t morton_encode(uint32_t x, uint32_t y, uint32_t z){
+    uint64_t answer = 0;
+    answer |= split_by_3(x) | split_by_3(y) << 1 | split_by_3(z) << 2;
+    return answer;
+}
+
+static void morton_decode(uint64_t morton, uint32_t* x, uint32_t* y, uint32_t* z) {
+    *x = compact_by_3(morton);
+    *y = compact_by_3(morton >> 1);
+    *z = compact_by_3(morton >> 2);
+}
 
 int32_t construct_svo(svo_node_t* dst, uint32_t max_nodes, uint32_t* voxels, uint32_t w) {
-    svo_node_t* scratch_arena = malloc(SCRATCH_SPACE_MULTIPLIER * max_nodes * sizeof(svo_node_t));
-    uint32_t num_alloc = 1;
-
-    PROPAGATE_CLEANUP_BEGIN(construct_svo_tree(scratch_arena, &num_alloc, 0, SCRATCH_SPACE_MULTIPLIER * max_nodes, voxels, w, 0, 0, 0) != SVO_ERROR, "Couldn't create initial SVO structure.");
-    free(scratch_arena);
-    PROPAGATE_CLEANUP_END(ERROR);
-
-    svo_queue_entry_t* queue = malloc(max_nodes * sizeof(svo_queue_entry_t));
-    queue[0]._index = 0;
-    queue[0]._type = PARENT;
-    uint32_t queue_head = 0, queue_tail = 1;
-    while (queue_head < queue_tail) {
-	if (queue[queue_head]._type == PARENT) {
-	    dst[queue_head]._parent._child_pointer = (uint16_t) queue_tail;
-	    uint8_t invalid_mask = scratch_arena[queue[queue_head]._index]._parent._invalid_mask;
-	    uint8_t leaf_mask = scratch_arena[queue[queue_head]._index]._parent._leaf_mask;
-	    dst[queue_head]._parent._invalid_mask = invalid_mask;
-	    dst[queue_head]._parent._leaf_mask = leaf_mask;
-	    for (uint8_t i = 0; i < 8; ++i) {
-		if (invalid_mask & 0x80) {
-		    queue[queue_tail]._index = scratch_arena[queue[queue_head]._index]._parent._child_pointer + i;
-		    queue[queue_tail]._type = (leaf_mask << i) & 0x80 ? LEAF : PARENT;
-		    ++queue_tail;
-		}
-		invalid_mask <<= 1;
-	    }
-	}
-	else if (queue[queue_head]._type == LEAF) {
-	    dst[queue_head]._raw = scratch_arena[queue[queue_head]._index]._raw;
-	}
-	++queue_head;
-    }
-
-    free(scratch_arena);
-    free(queue);
     return SUCCESS;
 }
 
